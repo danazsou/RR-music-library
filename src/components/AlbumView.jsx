a623cc89c4db3324be3c212130c0af0b78681cd7
danazou <dana.sou@gmail.com>
2022-12-18 15:40:02 -0800

    corrected some code errors and passed props

--- src/components/AlbumView.jsx
+++ src/components/AlbumView.jsx
@@ -1,10 +1,11 @@
 // These components will be making separate API calls from the app
-// component to serve specific data about our artist
+// component to serve specific data about a given album
+import { useState} from 'react'
 import { useParams } from 'react-router-dom'
 
 function AlbumView() {
     const { id } = useParams()
-    const [ albumData, setAlbumData ] = useState([])
+    const [ albumData, setalbumData ] = useState([])
 
     return (
         <div>
@@ -14,5 +15,4 @@ function AlbumView() {
     )
 }
 
-
-export default ArtistView
+export default AlbumView
--- src/components/ArtistView.jsx
+++ src/components/ArtistView.jsx
@@ -1,5 +1,6 @@
 // These components will be making separate API calls from the app
 // component to serve specific data about a given album
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 function ArtistView() {
@@ -14,3 +15,4 @@ function ArtistView() {
     )
 }
 
+export default ArtistView
