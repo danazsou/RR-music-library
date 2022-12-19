392a5ac92f8d5469cbeaeeaa082cbb97114caba4
danazou <dana.sou@gmail.com>
2022-12-18 20:12:21 -0800

    imported useNavigate and added nav buttons

--- src/components/AlbumView.jsx
+++ src/components/AlbumView.jsx
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 
 function AlbumView() {
+
     const { id } = useParams()
     const [ albumData, setAlbumData ] = useState([])
+    const navigate = useNavigate()
 
     useEffect(() => {
         const API_URL = `http://localhost:4000/song/${id}`
@@ -25,12 +27,24 @@ function AlbumView() {
         )
     })
 
+    const navButtons = () => {
+        return(
+            <div>
+                <button onClick={() => navigate(-1)}>Back</button>
+                |
+                <button onClick={() => navigate('/')}>Home</button>
+            </div>
+        )
+    }
+
     return (
         <div>
+            {navButtons()}
             {renderSongs}
         </div>
     )
 }
+
 
 
 export default AlbumView
