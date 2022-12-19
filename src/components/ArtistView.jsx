401de896916f5f68bb97aa5e47bb3192d9af773c
danazou <dana.sou@gmail.com>
2022-12-18 19:27:02 -0800

    fetch API data in ArtistView

--- src/components/ArtistView.jsx
+++ src/components/ArtistView.jsx
@@ -1,18 +1,20 @@
-// These components will be making separate API calls from the app
-// component to serve specific data about a given album
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 function ArtistView() {
     const { id } = useParams()
     const [ artistData, setArtistData ] = useState([])
 
-    return (
-        <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Artist Data Goes Here!</p>
-        </div>
-    )
+    useEffect(() => {
+        const API_URL = 'http://localhost:4000/album/${id}'
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            console.log(resData)
+        }
+        fetchData(id)
+    })
 }
+
 
 export default ArtistView
